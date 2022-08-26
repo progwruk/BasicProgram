7abf9354af36111ed73e11727c1af5bda2c0a8bd
progwruk <wmeshram@gmail.com>
2022-08-26 20:32:14 +0530

    [Wrukshali] Add: Welcome to basic core program

---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic core program");
+        }
+    }
+}
