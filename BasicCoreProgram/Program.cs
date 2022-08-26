df503f1e6615722442652a56aadd1f7fc654cd14
progwruk <wmeshram@gmail.com>
2022-08-26 20:51:04 +0530

    [Wrukshali] Add: Leap year

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -8,13 +8,17 @@ namespace BasicCoreProgram
             Console.WriteLine("Welcome to basic core program");
             while (true)
             {
-                Console.WriteLine("Select program\n 1.flip coin");
+                Console.WriteLine("Select program\n 1.flip coin\n 2.leap year");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         FlipCoin flipCoin = new FlipCoin();
                         flipCoin.Coin();
+                        break;
+                    case 2:
+                        LeapYear leapyear = new LeapYear();
+                        leapyear.Year();
                         break;
                     default:
                         Console.WriteLine("Select correct option");
