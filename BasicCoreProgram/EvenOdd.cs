bbe58b716ab620c6ce96a94b73b2411e072778c7
progwruk <wmeshram@gmail.com>
2022-08-26 21:59:53 +0530

    [Wrukshali] Add: Even odd

---
+++ BasicCoreProgram/EvenOdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class EvenOdd
+    {
+        public void Check()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("It is an even number", num);
+            else
+                Console.WriteLine("It is an odd number", num);
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -8,7 +8,7 @@ namespace BasicCoreProgram
             Console.WriteLine("Welcome to basic core program");
             while (true)
             {
-                Console.WriteLine("Select program\n 1.flip coin\n 2.leap year\n 3.power\n 4.harmonic number\n 5.factors\n 6.quotientRemainder\n 7.swap numbers");
+                Console.WriteLine("Select program\n 1.flip coin\n 2.leap year\n 3.power\n 4.harmonic number\n 5.factors\n 6.quotientRemainder\n 7.swap numbers\n 8.evenOdd");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -39,6 +39,10 @@ namespace BasicCoreProgram
                     case 7:
                         SwapNumbers swapNumbers = new SwapNumbers();
                         swapNumbers.Swap();
+                        break;
+                    case 8:
+                        EvenOdd evenOdd = new EvenOdd();
+                        evenOdd.Check();
                         break;
                     default:
                         Console.WriteLine("Select correct option");
