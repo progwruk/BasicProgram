27dc437a967fb21ad5efd148e4b655bf6de394fe
progwruk <wmeshram@gmail.com>
2022-08-26 20:41:17 +0530

    [Wrukshali] Add: Flip coin

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,6 +6,21 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic core program");
+            while (true)
+            {
+                Console.WriteLine("Select program\n 1.flip coin");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Coin();
+                        break;
+                    default:
+                        Console.WriteLine("Select correct option");
+                        break;
+                }
+            }
         }
     }
 }
